3bcbc8bdf338ccec757162dd25fb3ea76eb7b38d
LyckaWebshop <107996049+LyckaWebshop@users.noreply.github.com>
2023-05-31 09:05:00 +0200

    Update Intro2.jsx

--- src/components/Modal/Intro2.jsx
+++ src/components/Modal/Intro2.jsx
@@ -31,7 +31,7 @@ export default function Intro2({open, setOpen}) {
                             <br/>
                             <br/>
                             When arriving at your destination the airline tells you that your luggage will arrive after
-                            seven days, because they checked your luggage into another airplane.
+                            two days, because they checked your luggage into another airplane.
                             For you to have clothes for the next two days, the airline offers you the following:
                             <br/>
                             <br/>
