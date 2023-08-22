74c0338b75f5f2d654bc4ae3e107e5e59901797a
jcschn <125440054+jcschn@users.noreply.github.com>
2023-08-22 13:20:36 +0200

    newJCS

--- src/components/Modal/intro2b.jsx
+++ src/components/Modal/intro2b.jsx
@@ -5,9 +5,11 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import Intro2 from "./Intro2";
+import FormDialog from "./FormDialog";
+import Intro3 from "./Intro3";
 
-export default function Intro1({open, setOpen}) {
+
+    export default function Intro2b({open, setOpen}) {
     const [rOpen, rSetOpen] = React.useState(false);
     const handleClose = () => {
         setOpen(false);
@@ -17,22 +19,20 @@ export default function Intro1({open, setOpen}) {
         rSetOpen(true);
     };
 
+
+
+
     return (
         <div>
-            <Intro2 open={rOpen} setOpen={rSetOpen}/>
-            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-                <DialogTitle id="form-dialog-title">Welcome back!</DialogTitle>
+            <Intro3 open={rOpen} setOpen={rSetOpen}/>
+           <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">How you will be paid</DialogTitle>
                 <DialogContent>
                     <DialogContentText>
                         <p>
-                            Welcome back and thank you for participating &#8212; your assistance is greatly appreciated!
-                            <br/> <br/>
-                            All your answers will be treated anonymously, with strict confidentiality, and will be used
-                            for research purposes only.
-                            <br/> <br/>
-                            A note on how to participate:
-                            <br/><br/>
-                            (1)	Please do not click return or refresh in your browser during the participation!
+                             For this survey you will receive a fixed and a variable payment which depends on your decisions
+                            <br/>
+                            Your fixed payment is 1.5 £. Your variable payment corresponds to one of the items in your basket. Which item is determined randomly. The price of the randomly selected item will then be transferred to real money. For instance, a price of 30€ in the online shop corresponds to an amount of 30*0.015=0.45 £ of real money. This is your variable payment.
                             <br/>
                             <br/>
                             Thank you very much for your participation and your valuable time!
