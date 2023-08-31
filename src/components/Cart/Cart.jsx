adab92a516d040268f35649785f5aa047ee627a4
jcschn <125440054+jcschn@users.noreply.github.com>
2023-08-31 14:00:37 +0200

    newJCS

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -142,15 +142,15 @@ const Cart = ({cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart, onAddSustai
                 subtotal: cart.subtotal.raw,
                 products: products
             };
-             COMMENT IN FOR DATA PUSH TO SERVERS
-          *  axios.post("https://eu-central-1.aws.data.mongodb-api.com/app/application-0-vxthq/endpoint/postData",
+             axios.post("https://eu-central-1.aws.data.mongodb-api.com/app/application-0-vxthq/endpoint/postData",
                 jsonObj).then(res => {
                 console.log(res);
-            });*/
-            COMMENT IN FOR DATA PUSH TO SERVERS
-        handleClickOpen();
-        setOpen(true);
-         });
+                handleClickOpen();
+                setOpen(true);
+            });
+        } else {
+            alert("Your cart's subtotal is bigger than 200€, please remove products");
+        }
     };
 
     const handleClickOpen = () => {
